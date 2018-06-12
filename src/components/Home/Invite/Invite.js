517c4ddaf8c256796d1702ae9f2db6b1b54cb605
DJLionheart <aundario@gmail.com>
2018-06-12 16:37:48 -0600

    home style updates

--- src/components/Home/Invite/Invite.js
+++ src/components/Home/Invite/Invite.js
@@ -4,33 +4,48 @@ import IconButton from '../../buttons/IconButton/IconButton';
 
 import acceptIcon from '../../../assets/svg/accept.svg';
 import declineIcon from '../../../assets/svg/decline.svg';
+import { mediaQueries } from '../../styledComponents';
 
 
 const InviteContainer = glamorous.div({
-    padding: 5,
+    height: 60,
+    padding: 3,
     width: 210,
     marginBottom: 10,
     border: '1px solid',
     borderRadius: 3,
     boxShadow: '1px 1px 3px grey',
     fontFamily: 'Open Sans, sans-serif',
+    backgroundColor: '#9ebfe6',
     ':hover': {
 
+    },
+    [mediaQueries.iPhone678]: {
+        marginBottom: 16
     }
 }, ({ theme }) => ({
     borderColor: theme.mainBg,
-    backgroundColor: theme.newBlue
+    // backgroundColor: theme.newBlue
 }))
 
 const InviteTextP = glamorous.p({
-    
+    float: 'left',
+    lineHeight: '1em',
+    ':nth-child(odd)': {
+        fontWeight: 800,
+        fontSize: 18
+    }
 }, ({ theme }) => ({
-    color: theme.lighterText
+    color: theme.lighterText,
+    ':last-child': {
+        color: theme.sunglow
+    }
 }))
 
 const InviteControls = glamorous.span({
+    cursor: 'pointer',
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     alignItems: 'center'
 })
 
@@ -40,7 +55,9 @@ function Invite(props) {
 
     return(
         <InviteContainer>
-            <InviteTextP> <b>{ `${first_name + ' ' + last_name}`}</b>{ `has invited you to ${trip_name}` }</InviteTextP>
+            <InviteTextP>{ `${first_name + ' ' + last_name}`}</InviteTextP>
+            <InviteTextP>has invited you to</InviteTextP>
+            <InviteTextP>{`${trip_name}`}</InviteTextP>
             <InviteControls>
                 <img src={ acceptIcon } alt="accept invite" width="25px" onClick={ () => accept(tripid) }/>
                 <img src={ declineIcon } alt="decine invite" width="25px" onClick={ () => decline(tripid) }/>
