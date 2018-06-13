213409e704366dd8a7ce7f8529d7bcc1ea0b1fbd
Cody Tomesch <tomeschcody@gmail.com>
2018-06-13 10:31:16 -0600

    fixed search box dropdown on map

--- src/components/Trip/Map/SearchBox.js
+++ src/components/Trip/Map/SearchBox.js
@@ -64,7 +64,7 @@ const SelectAndButton = glamorous.div({
 
 const SearchSuggestions = glamorous.div({
   position: "fixed",
-  top: "91px",
+  top: "101px",
   left: "10px",
   zIndex: "1",
   borderRadius: "15px",
