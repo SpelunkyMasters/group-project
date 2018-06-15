3f19df1373d849697f366f639d4f647b13d6d819
DJLionheart <aundario@gmail.com>
2018-06-14 23:32:26 -0600

    getInvites unit tests

---
+++ src/__test__/getInvites.test.js
@@ -0,0 +1,35 @@
+let axios = require('axios');
+
+describe("Get Invite Action Creator Tests", () => {
+    const GET_INVITES = 'GET_INVITES';
+
+    function getInvites(userid) {
+        let invites = axios.get(`/api/invites/${userid}`).then( res => {
+            return res.data
+        }).catch(() => { console.log('getInvites request sent')})
+        return {
+            type: GET_INVITES,
+            payload: invites
+        }
+    }
+
+    test("getInvites function should return an action with type and payload properties", () => {
+        let flag = true;
+        let id = 2;
+
+        let action = getInvites(id)
+
+        if(!action.type || !action.payload) { flag = false };
+
+        expect(flag).toBeTruthy();
+    });
+
+    test("Payload of getInvites action should contain an empty object / promise", () => {
+        let id = 1;
+
+        let action = getInvites(id);
+        console.log(action.payload)
+
+        expect(typeof action.payload).toEqual('object')
+    })
+})
